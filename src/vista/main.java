e276296ccfd840130ba8a4e23bafc58634c9ba50
cdionicio <carlitosd1988@gmail.com>
2024-08-03 18:09:30 -0500

    primer_commit

---
+++ src/vista/main.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package vista;
+
+/**
+ *
+ * @author CARLOS
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+frm_calcular frm = new frm_calcular();
+frm.show();
+
+calcular repro = new calcular();
+
+
+
+    }
+
+    private static class calcular {
+
+        public calcular() {
+        }
+    }
+    
+            
+}
